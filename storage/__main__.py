10ada238709d3c496081f042c2ba154e46ceb269
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-10-10 21:02:58 +0600

    Settings from environment variables

--- storage/__main__.py
+++ storage/__main__.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from storage.config import settings
 from storage.web.api import api_router
 
 if __name__ == "__main__":
@@ -14,4 +15,9 @@ if __name__ == "__main__":
         allow_headers=["*"],
     )
     app.include_router(api_router)
-    uvicorn.run(app, host="0.0.0.0", port=8000, root_path="/storage")
+    uvicorn.run(
+        app,
+        host=str(settings.SERVER_HOST),
+        port=settings.SERVER_PORT,
+        root_path=settings.SERVER_ROOT_PATH,
+    )
---
+++ storage/config.py
@@ -0,0 +1,16 @@
+from ipaddress import IPv4Address
+from typing import Literal
+
+from pydantic import AnyHttpUrl, BaseSettings
+
+
+class Settings(BaseSettings):
+    SERVER_HOST: AnyHttpUrl | IPv4Address | Literal["localhost"]
+    SERVER_PORT: int
+    SERVER_ROOT_PATH: str = ""
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
