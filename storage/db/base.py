1dc541cd8b338e4e4a58c251f1ded9f943ea1ea0
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-11-04 12:46:52 +0600

    Remove dummy tenant model

--- storage/db/base.py
+++ storage/db/base.py
@@ -1,5 +1,3 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 from storage.db.base_class import Base  # noqa
-
-from .models.tenant import *  # noqa
--- storage/db/models/tenant.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer
-
-from storage.db.base_class import Base
-
-
-class Tenant(Base):
-    __tablename__ = "tenants"
-
-    id = Column(Integer, primary_key=True, index=True)
