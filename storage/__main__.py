5c8578f609a507be3ab166d2c7a12d0a37ee8fa6
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-11-05 01:41:17 +0600

    Tenant create and API key create CLI

--- storage/__main__.py
+++ storage/__main__.py
@@ -7,10 +7,13 @@ from uvicorn import Config, Server
 
 from storage import db
 from storage.config import settings
+from storage.db.models.tenant import Tenant
+from storage.db.models.token import Token
 from storage.db.multitenancy import tenant_create
-from storage.db.session import SessionLocal, engine
+from storage.db.session import SessionLocal, engine, with_db
 from storage.logging import log, setup_logging
 from storage.web.api import api_router
+from storage.web.security import create_api_key, get_api_key_hash
 
 
 def pre_start():
@@ -35,15 +38,40 @@ if __name__ == "__main__":
         "--pre-start", help="execute preliminary routine and exit", action="store_true"
     )
     group.add_argument(
-        "--tenant-create", help="creates new tenant", dest="tenant", type=str
+        "--tenant-create", help="creates new tenant", dest="new_tenant_name"
+    )
+    group.add_argument(
+        "--create-api-key-for-tenant",
+        help="create API key token for a tenant",
+        dest="existing_tenant_name",
     )
     args = parser.parse_args()
     if args.pre_start:
         pre_start()
         exit(0)
-    if args.tenant:
-        tenant_create(args.tenant, args.tenant, args.tenant)
-        log.info(f"new tenant created, {args.tenant=}")
+    if args.new_tenant_name:
+        tenant_create(args.new_tenant_name, args.new_tenant_name, args.new_tenant_name)
+        log.info(f"new tenant created, {args.new_tenant_name=}")
+        exit(0)
+    if args.existing_tenant_name:
+        with with_db() as database:
+            tenant = (
+                database.query(Tenant)
+                .filter(Tenant.name == args.existing_tenant_name)
+                .first()
+            )
+        if not tenant:
+            log.error(f"can't create api key, tenant not exists, {tenant=}")
+            exit(1)
+        api_key = create_api_key()
+        token = Token(
+            hashed_token=get_api_key_hash(api_key),
+            owner_id=tenant.id,
+        )
+        with with_db() as database:
+            database.add(token)
+            database.commit()
+        print(api_key)
         exit(0)
 
     app = FastAPI(title="MerkleBot Storage")
