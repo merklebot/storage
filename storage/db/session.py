a26e5191b4b40e6878e668a2b53757f01c46aae0
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-11-05 01:32:07 +0600

    Context manager for tenant-specific database conn

--- storage/db/session.py
+++ storage/db/session.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -5,3 +7,19 @@ from storage.config import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@contextmanager
+def with_db(tenant_schema: str | None = None):
+    if tenant_schema:
+        schema_translate_map = dict(tenant=tenant_schema)
+    else:
+        schema_translate_map = None
+
+    connectable = engine.execution_options(schema_translate_map=schema_translate_map)
+
+    try:
+        db = SessionLocal(bind=connectable)
+        yield db
+    finally:
+        db.close()
