336ca70c825712428a9d833a4ef2a3b2f1f507fd
Arseniy <arseniypopugay@gmail.com>
2022-11-06 15:48:18 +0300

    Update permission schema

--- storage/web/schemas/permission.py
+++ storage/web/schemas/permission.py
@@ -1,8 +1,17 @@
 from fastapi_camelcase import CamelModel as BaseModel
+from enum import Enum
+
+
+class PermissionKind(str, Enum):
+    get = "get"
+    update = "update"
 
 
 class PermissionBase(BaseModel):
     content_id: int
+    owner_id: int
+    assignee_id: int
+    kind: PermissionKind
 
 
 class PermissionCreate(PermissionBase):
