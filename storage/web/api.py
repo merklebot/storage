4fd900f123bc5d12c9fc8688c88d81c065af61f2
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-11-14 16:21:09 +0600

    Alphabetical sorting for tags in Swagger UI

--- storage/web/api.py
+++ storage/web/api.py
@@ -12,15 +12,15 @@ from storage.web.endpoints import (
 )
 
 api_router = APIRouter()
-api_router.include_router(tenants.router, prefix="/tenants", tags=["Tenants"])
-api_router.include_router(users.router, prefix="/users", tags=["Users"])
-api_router.include_router(keys.router, prefix="/keys", tags=["Keys"])
 api_router.include_router(contents.router, prefix="/contents", tags=["Contents"])
-api_router.include_router(
-    specifications.router, prefix="/specifications", tags=["Specifications"]
-)
+api_router.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
+api_router.include_router(keys.router, prefix="/keys", tags=["Keys"])
 api_router.include_router(
     permissions.router, prefix="/permissions", tags=["Permissions"]
 )
-api_router.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
-api_router.include_router(tokens.router, prefix="/tokens", tags=["Auth tokens"])
+api_router.include_router(
+    specifications.router, prefix="/specifications", tags=["Specifications"]
+)
+api_router.include_router(tenants.router, prefix="/tenants", tags=["Tenants"])
+api_router.include_router(tokens.router, prefix="/tokens", tags=["Tokens"])
+api_router.include_router(users.router, prefix="/users", tags=["Users"])
