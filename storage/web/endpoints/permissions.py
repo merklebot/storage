4e1916e3d89d0ce309b6a6eac058319af726eeb7
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-11-15 13:41:09 +0600

    Use database in permission resource handlers

--- storage/web/endpoints/permissions.py
+++ storage/web/endpoints/permissions.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 
+from storage.db.models import Content, Permission
+from storage.db.session import SessionLocal
 from storage.logging import log
 from storage.web import deps
 from storage.web.schemas import permission as schemas
@@ -11,87 +13,113 @@ router = APIRouter()
 @router.get("/", response_model=list[schemas.Permission])
 async def read_permissions(
     *,
-    db: dict = Depends(deps.get_fake_db),
+    db: SessionLocal = Depends(deps.get_db),
 ):
-    return list(db["permissions"].values())
+    log.debug("read_permissions")
+    permissions = db.query(Permission).all()
+    return permissions
 
 
 @router.post("/", response_model=schemas.Permission)
 async def create_permission(
     *,
-    db: dict = Depends(deps.get_fake_db),
-    permission_in: schemas.PermissionCreate,
+    db: SessionLocal = Depends(deps.get_db),
     user_id: int,
+    permission_in: schemas.PermissionCreate,
 ):
-
-    permissions = deps.get_permission_for_user_by_content_id(
-        db, content_id=permission_in.content_id, user_id=user_id
+    log.debug(f"create_permission, {permission_in=}, {user_id=}")
+    content: Content | None = (
+        db.query(Content).filter(Content.id == permission_in.content_id).first()
     )
-    if not permissions.owner:
+    if not content:
         raise HTTPException(
-            status_code=403, detail="No permission to create permission"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Content not found"
         )
-    log.debug(f"create_permission, {permission_in=}")
-    permission = schemas.Permission(
-        id=max(db["permissions"].keys()) + 1 if db["permissions"].keys() else 0,
-        **permission_in.dict(),
+    if user_id != content.owner_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions"
+        )
+    permission = Permission(
+        kind=permission_in.kind,
+        content_id=permission_in.content_id,
+        assignee_id=permission_in.assignee_id,
     )
-    db["permissions"][permission.id] = permission.dict()
+    db.add(permission)
+    db.commit()
+    db.refresh(permission)
     return permission
 
 
 @router.get("/{permission_id}", response_model=schemas.Permission)
 async def read_permission_by_id(
     *,
-    db: dict = Depends(deps.get_fake_db),
+    db: SessionLocal = Depends(deps.get_db),
     permission_id: int,
 ):
     log.debug(f"read_permission_by_id, {permission_id=}")
-    try:
-        return db["permissions"][permission_id]
-    except KeyError:
-        raise HTTPException(status_code=404, detail="Permission not found")
+    permission: Permission | None = (
+        db.query(Permission).filter(Permission.id == permission_id).first()
+    )
+    if not permission:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Permission not found"
+        )
+    return permission
 
 
 @router.put("/{permission_id}", response_model=schemas.Permission)
 async def update_permission(
     *,
-    db: dict = Depends(deps.get_fake_db),
+    db: SessionLocal = Depends(deps.get_db),
+    user_id: int,
     permission_id: int,
     permission_in: schemas.PermissionUpdate,
-    user_id: int,
 ):
-    permissions = deps.get_permission_for_user_by_content_id(
-        db, content_id=permission_in.content_id, user_id=user_id
+    log.debug(f"update_permission, {permission_in=}, {permission_id=}, {user_id=}")
+    permission: Permission | None = (
+        db.query(Permission).filter(Permission.id == permission_id).first()
     )
-    if not (permissions.owner or permissions.update):
+    if not permission:
         raise HTTPException(
-            status_code=403, detail="No permission to update permission"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Permission not found"
         )
-    log.debug(f"update_permission, {permission_id=}, {permission_in=}")
-    if permission_id not in db["permissions"]:
-        raise HTTPException(status_code=404, detail="Permission not found")
-    permission = db["permissions"][permission_id]
-    log.debug(permission)
-    permission.update({k: v for k, v in permission_in.dict().items() if v is not None})
-    db["permissions"][permission_id] = permission
+    content: Content = (
+        db.query(Content).filter(Content.id == permission.content_id).first()
+    )
+    if user_id != content.owner_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions"
+        )
+    permission.assignee_id = permission.assignee_id
+    permission.content_id = permission_in.content_id
+    permission.kind = permission_in.kind
+    db.commit()
+    db.refresh(permission)
     return permission
 
 
 @router.delete("/{permission_id}", response_model=schemas.Permission)
 async def delete_permission(
     *,
-    db: dict = Depends(deps.get_fake_db),
-    permission_id: int,
+    db: SessionLocal = Depends(deps.get_db),
     user_id: int,
+    permission_id: int,
 ):
-    log.debug(f"delete_permission, {permission_id=}")
-    if permission_id not in db["permission"]:
-        raise HTTPException(status_code=404, detail="Permission not found")
-    permission = db["permissions"].pop(permission_id)
-    if not permission["owner_id"] != user_id:
+    log.debug(f"delete_permission, {permission_id=}, {user_id=}")
+    permission: Permission | None = (
+        db.query(Permission).filter(Permission.id == permission_id).first()
+    )
+    if not permission:
         raise HTTPException(
-            status_code=403, detail="No permission to delete permission"
+            status_code=status.HTTP_404_NOT_FOUND, detail="Permission not found"
         )
-    log.debug(permission)
+    content: Content = (
+        db.query(Content).filter(Content.id == permission.content_id).first()
+    )
+    if user_id != content.owner_id:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not enough permissions"
+        )
+    db.delete(permission)
+    db.commit()
     return permission
