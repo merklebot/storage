be9f19cdf09e801a234dca011e474d3e047dacf3
alex <alexander.smehnov@gmail.com>
2023-06-08 15:45:10 +0300

    Download file endpoint returns temporary link

--- storage/services/instant_storage.py
+++ storage/services/instant_storage.py
@@ -14,3 +14,23 @@ async def upload_data_to_instant_storage(data, ipfs_cid: str):
         await s3.put_object(
             Bucket=settings.INSTANT_STORAGE_BUCKET_NAME, Key=f"{ipfs_cid}", Body=data
         )
+
+
+async def generate_access_link_for_instant_storage_data(ipfs_cid: str):
+    expires_in = 3600
+    async with aioboto3.Session().client(
+        service_name="s3",
+        region_name=settings.INSTANT_STORAGE_REGION,
+        endpoint_url=settings.INSTANT_STORAGE_ENDPOINT,
+        aws_access_key_id=settings.INSTANT_STORAGE_ACCESS_KEY,
+        aws_secret_access_key=settings.INSTANT_STORAGE_SECRET_ACCESS_KEY,
+    ) as s3:
+        res = await s3.generate_presigned_url(
+            ClientMethod="get_object",
+            Params={
+                "Bucket": settings.INSTANT_STORAGE_BUCKET_NAME,
+                "Key": f"{ipfs_cid}",
+            },
+            ExpiresIn=expires_in,
+        )
+        return res, expires_in
--- storage/web/routers/contents.py
+++ storage/web/routers/contents.py
@@ -1,7 +1,7 @@
 import httpx
 from fastapi import APIRouter, BackgroundTasks, Depends, Request, UploadFile, status
 from fastapi.exceptions import HTTPException
-from fastapi.responses import RedirectResponse, StreamingResponse
+from fastapi.responses import RedirectResponse
 
 from storage.config import settings
 from storage.db.models import Content, Permission, User
@@ -10,7 +10,10 @@ from storage.db.models.permission import PermissionKind
 from storage.db.session import SessionLocal
 from storage.logging import log
 from storage.schemas import content as schemas
-from storage.services.instant_storage import upload_data_to_instant_storage
+from storage.services.instant_storage import (
+    generate_access_link_for_instant_storage_data,
+    upload_data_to_instant_storage,
+)
 from storage.upload import process_data_from_origin
 from storage.web import deps
 
@@ -216,7 +219,7 @@ async def download_content_file(
     current_user: User = Depends(deps.get_current_user),
     content_id: int,
 ):
-    """Read content file the user owns or has permission to read."""
+    """Returns temporary link to file the user owns or has permission to read."""
 
     log.debug(f"download_content_file, {content_id=}, {current_user.id=}")
     content: Content = db.query(Content).filter(Content.id == content_id).first()
@@ -252,62 +255,7 @@ async def download_content_file(
             detail="Unavailable by unknown reason",
         )
 
-    filename = content.filename or content.origin.split("/")[-1]
-    url = f"{settings.IPFS_HTTP_PROVIDER}/api/v0/get?arg={content.ipfs_cid}"
-    print(url)
-    client = httpx.AsyncClient()
-
-    async def iterative_download(download_url, async_client):
-        # download file with on-the-fly tar unpacking
-        try:
-            async with async_client.stream("POST", download_url) as response:
-                start_bytes = []
-                current_length = 0
-                size_read_flag = False
-                SIZE_OFFSET = 124  # start position of filesize bytes
-                SZ_SIZE = 12  # length of filesize bytes
-                HEADER_LENGTH = 512  # length of tar header
-                async for chunk in response.aiter_bytes():
-                    current_length += len(chunk)
-                    if current_length < HEADER_LENGTH:
-                        start_bytes += chunk
-                        continue
-
-                    if not size_read_flag:
-                        start_bytes += chunk
-                        filesize_oct_str = "".join(
-                            [
-                                chr(byte_code)
-                                for byte_code in start_bytes[
-                                    SIZE_OFFSET : SIZE_OFFSET + SZ_SIZE - 1
-                                ]
-                            ]
-                        )
-                        filesize = int(filesize_oct_str, 8)
-                        size_read_flag = True
-                        yield bytes(start_bytes[HEADER_LENGTH:])
-                        continue
-
-                    if current_length > filesize + HEADER_LENGTH:
-                        yield bytes(
-                            chunk[
-                                : (filesize + HEADER_LENGTH)
-                                - (current_length - len(chunk))
-                            ]
-                        )
-                        break
-                    yield chunk
-        except Exception as e:
-            import traceback
-
-            traceback.print_exc()
-            log.error(f"download_content_file, {e=} {content.id=}")
-            raise HTTPException(
-                status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                detail="IPFS provider failure",
-            )
-
-    return StreamingResponse(
-        iterative_download(url, client),
-        headers={"Content-Disposition": f"attachment;filename={filename}"},
+    presigned_url, expires_in = await generate_access_link_for_instant_storage_data(
+        content.ipfs_cid
     )
+    return {"url": presigned_url, "expires_in": expires_in}
